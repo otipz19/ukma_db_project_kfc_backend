766032a477870b99c0c5f06e30c3d8b9423c6957
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2025-03-02 06:30:18 +0200

    Initial commit

---
+++ src/main/java/ua/edu/ukma/db/kfc/KFCApplication.java
@@ -0,0 +1,9 @@
+package ua.edu.ukma.db.kfc;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class KFCApplication extends Application {
+}
